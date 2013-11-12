4615a50df48eb34cc88569181598fb4e7199d4ff
jellydn <a12pct@gmail.com>
2013-11-12 17:38:54 +0900

    list all sub category

--- wp-content/themes/beautysimple/category.php
+++ wp-content/themes/beautysimple/category.php
@@ -25,28 +25,38 @@ get_header(); ?>
 <header class="mainColHeader">
 <ol class="topicPath">
 <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">TOP</a></li>
+<?php if (is_category('health' )) { ?>
 <li>美容と健康</li>
+<?php } else if (is_category('cosme' )) { ?>
+<li>お悩み・効果</li>
+<?php } else if (is_category('trouble' )) { ?>
+<li>お悩み・効果</li>
+<?php } elseif (is_category('component' )) { ?>
+<li>成分・特長</li>
+<?php } ?>
 </ol>
-
 <div class="pageTtl">
+<?php if (is_category('health' )) { ?>
 <h1>美容と健康</h1>
-</div>
+<?php } elseif (is_category('cosme' )) { ?>
+<h1>お悩み・効果</h1>
+<?php } elseif (is_category('trouble' )) { ?>
+<h1>お悩み・効果</h1>
+<?php } elseif (is_category('component' )) { ?>
+<h1>成分・特長</h1>
+<?php } ?></div>
 
 <div class="pageOverview">
-<p>グラビアアイドルやモデルさんみたいにキュッと引き締まったヒップになりたい！</p>
-<p>でも現実はセルライトがたくさんついてぶよぶよ、、そんなお悩みに、簡単にできるリンパマッサージのやり方をご紹介します！</p>
+ <?php echo category_description( ); ?> 
+
 </div>
 
 <div class="tagLinks"><span>カテゴリ：</span>
-<a href="tag.html">ダイエット</a>
-<a href="#">運動</a>
-<a href="#">イベント</a>
-<a href="#">食生活</a>
-<a href="#">酵素</a>
-<a href="#">サプリメント</a>
-<a href="#">便秘解消</a>
-<a href="#">冷え性</a>
-<a href="#">花粉症</a>
+<?php // list child categories
+ $this_cat = get_query_var('cat'); // get the category of this category archive page
+  the_category(' ' , $this_cat ); // list child categories
+?>
+
 </div>
 
 </header>
