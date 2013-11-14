e0b4e4fa21433026b4c8cc939118ddd03e6f8d3f
jellydn <a12pct@gmail.com>
2013-11-14 16:52:34 +0900

    Finish google custom search

--- wp-content/themes/beautysimple/searchform.php
+++ wp-content/themes/beautysimple/searchform.php
@@ -4,3 +4,30 @@
 	</label>
 	<a href="#" class="searchBtn">検索</a>
 </form>
+<div style="display:none">
+<?php
+display_search_box(DISPLAY_RESULTS_AS_POP_UP);
+?>
+<script type="text/javascript">
+jQuery(document).ready(function($){
+	$('.search-form').submit(function(event) {
+		do_custom_search();
+		return false;
+	});
+
+	$('.searchBtn').click(function(event) {
+		do_custom_search();
+		return false;
+	});
+
+	function do_custom_search()
+	{
+		// alert($('.search-field').val());
+		$('.gsc-input input').val($('.search-field').val());
+		$('.gsc-search-button input[type=button]').click();
+	}
+
+});
+
+</script>
+</div>
